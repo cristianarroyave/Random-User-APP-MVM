67c774871dcacad6848d1ac41375fbe39288e650
cristianarroyave <cristian.arroyave.clavijo@gmail.com>
2022-04-15 15:54:59 +0200

    limpiar userdetailscreen imports

--- app/src/main/java/com/example/mvvmaristi/ui/view/Screens/UserDetailScreen.kt
+++ app/src/main/java/com/example/mvvmaristi/ui/view/Screens/UserDetailScreen.kt
@@ -1,22 +1,13 @@
 package com.example.mvvmaristi.ui.view.Screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
